2cae96ddc6af83c0556bdbb46a60e27332f27ed0
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-12-13 19:01:54 +0000

    watch

--- src/js/server.js
+++ src/js/server.js
@@ -5,57 +5,42 @@ import React from "react";
 import {renderToString} from "react-dom/server";
 import {match, RoutingContext} from "react-router";
 import express from "express";
-import webpack from "webpack";
-import webpackDevMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
-import config from "../../webpack.config";
-
+import piping from "piping";
 import routes from "./routes";
 
-const app = express();
-const compiler = webpack(config);
 
-app.use(webpackDevMiddleware(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-app.use(webpackHotMiddleware(compiler));
+if (piping({hook: true})) {
+  const app = express();
 
-app.use(webpackDevMiddleware(compiler, {
-  noInfo: false,
-  stats: {colors: true}
-}));
-
-app.use(webpackHotMiddleware(compiler));
-
-app.get("*", (req, res) => {
-  const location = req.originalUrl || "/";
-  match({routes, location}, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(505).send(error);
-    } else if (redirectLocation) {
-      const redirectPath = redirectLocation.pathname + redirectLocation.search;
-      res.redirect(redirectPath);
-    } else if (renderProps) {
-      res.send(body(renderProps))
-    } else {
-      res.status(404).send("Not found");
-    }
+  app.get("*", (req, res) => {
+    const location = req.originalUrl || "/";
+    match({routes, location}, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.status(505).send(error);
+      } else if (redirectLocation) {
+        const redirectPath = redirectLocation.pathname + redirectLocation.search;
+        res.redirect(redirectPath);
+      } else if (renderProps) {
+        res.send(body(renderProps))
+      } else {
+        res.status(404).send("Not found");
+      }
+    });
   });
-});
 
-app.listen(3000, 'localhost', error => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log('Listening at http://localhost:3000');
-});
+  app.listen(3000, 'localhost', error => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log('Listening at http://localhost:3000');
+  });
 
 
-function body(renderProps) {
-  var reactString = renderToString(<RoutingContext {...renderProps}/>);
-  const output = `<!doctype html>
+
+  function body(renderProps) {
+    var reactString = renderToString(<RoutingContext {...renderProps}/>);
+    const output = `<!doctype html>
 				<html lang='en-us'>
 					<head>
 						<meta charset='utf-8'>
@@ -63,8 +48,10 @@ function body(renderProps) {
 					</head>
 					<body>
 						<div id="app">${reactString}</div>
-						<script src="/assets/bundle.js"></script>
+						<script src="http://localhost:8080/assets/bundle.js"></script>
 					</body>
 				</html>`;
-  return output;
+    return output;
+  }
+
 }
